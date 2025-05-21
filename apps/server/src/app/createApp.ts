830f0fd7b76fcde7dce76deb44336dd1b181914b
Run Qi (Jack) Li <li.jack0707@gmail.com>
2025-05-20 21:37:07 -0400

    Create /register endpoint
    - create authRouter and /register route
    
    - create registerUser controller and add validation logic
    
    - create zod schema for register user and validate request body
    
    - added logic to check for duplicate username
    
    - Added express-rate-limit to throttle excessive creation requests
    
    - renamed server side index.ts to createApp.ts and moved to dedicated /app folder

--- apps/server/src/app/createApp.ts
+++ apps/server/src/app/createApp.ts
@@ -1,16 +1,16 @@
-import bodyParser from 'body-parser';
 import express from 'express';
 import type { Express } from 'express';
 
-import userRouter from './src/routes/user.routes';
+import authRouter from '../routes/auth.routes';
+import userRouter from '../routes/user.routes';
 
 export default function createApp(): Express {
   const app = express();
 
-  app.use(bodyParser.json());
-  app.use(express.json()); // Middleware to parse JSON request bodies
+  app.use(express.json());
 
   app.use('/api/v1/', userRouter);
+  app.use('/api/v1/auth', authRouter);
 
   app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
