1c12e838a5bd2faa9720bb4ca7c81281f2ebed88
Run Qi (Jack) Li <li.jack0707@gmail.com>
2025-05-25 21:49:23 -0400

    WIP - add zustand

---
+++ apps/client/src/api/constants.js
@@ -0,0 +1,9 @@
+// src/api/constants.js
+
+// Authentication Endpoints
+export const AUTH_ENDPOINTS = {
+  LOGIN: '/auth/login',
+  REGISTER: '/auth/register',
+  ME: '/auth/me',
+  LOGOUT: '/auth/logout',
+};
