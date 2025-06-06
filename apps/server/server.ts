a0e1e0d65836e73a833606e2149ae040f0a8c9bd
kayoMichael <michaelxkayo@hotmail.com>
2025-06-06 00:07:18 -0400

    Refactor CORS config

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -5,5 +5,5 @@ const app = createApp();
 const PORT = env.NODE_ENV == 'production' ? 8080 : 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- apps/server/src/config/cors.ts
+++ apps/server/src/config/cors.ts
@@ -1,7 +1,12 @@
 import { env } from '../schemas/env';
 
 export const corsOptions = {
-  origin: env.NODE_ENV === 'development' ? true : [process.env.CLIENT_HOST],
+  origin:
+    env.NODE_ENV !== 'production'
+      ? true
+      : [process.env.CLIENT_HOST].filter((host): host is string =>
+          Boolean(host),
+        ),
   credentials: true,
   optionsSuccessStatus: 200,
 };
