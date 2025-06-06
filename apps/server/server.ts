d88746e230615d0f3d59ed54d0c43ebf7ee6e4b7
kayoMichael <michaelxkayo@hotmail.com>
2025-06-06 19:25:32 -0400

    Refactor Client host

--- apps/server/server.ts
+++ apps/server/server.ts
@@ -5,5 +5,7 @@ const app = createApp();
 const PORT = env.NODE_ENV == 'production' ? 8080 : 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(
+    `Server is running on port ${PORT}. Cors set to ${process.env.CLIENT_HOST}`,
+  );
 });
