bee8b6b6f9ace60cbfc8b2afa2061f9970a0d4e9
kayoMichael <michaelxkayo@hotmail.com>
2025-06-06 20:08:49 -0400

    Configure CORS

--- apps/server/src/config/cors.ts
+++ apps/server/src/config/cors.ts
@@ -2,7 +2,9 @@ import { env } from '../schemas/env';
 
 export const corsOptions = {
   origin:
-    env.NODE_ENV !== 'production' || !process.env.CLIENT_HOST
+    env.NODE_ENV !== 'production' ||
+    process.env.CLIENT_HOST === 'ALLOW_ALL' ||
+    !process.env.CLIENT_HOST
       ? true
       : [process.env.CLIENT_HOST].filter((host): host is string =>
           Boolean(host),
