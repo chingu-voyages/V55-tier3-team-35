6d796362d17ff27684ff6aab933f6b6befaada19
kayoMichael <michaelxkayo@hotmail.com>
2025-06-06 21:08:35 -0400

    Add trust proxy true

--- apps/server/src/config/cors.ts
+++ apps/server/src/config/cors.ts
@@ -1,5 +1,14 @@
+import { env } from '../schemas/env';
+
 export const corsOptions = {
-  origin: true,
+  origin:
+    env.NODE_ENV !== 'production' ||
+    process.env.CLIENT_HOST === 'ALLOW_ALL' ||
+    !process.env.CLIENT_HOST
+      ? true
+      : [process.env.CLIENT_HOST].filter((host): host is string =>
+          Boolean(host),
+        ),
   credentials: true,
   optionsSuccessStatus: 200,
 };
