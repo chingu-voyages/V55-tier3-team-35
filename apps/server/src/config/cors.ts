bd48e44038d59e5572ec3a3b75c4f9e3f6d1b3eb
kayoMichael <michaelxkayo@hotmail.com>
2025-06-06 20:16:30 -0400

    Turn off CORS

--- apps/server/src/config/cors.ts
+++ apps/server/src/config/cors.ts
@@ -1,14 +1,5 @@
-import { env } from '../schemas/env';
-
 export const corsOptions = {
-  origin:
-    env.NODE_ENV !== 'production' ||
-    process.env.CLIENT_HOST === 'ALLOW_ALL' ||
-    !process.env.CLIENT_HOST
-      ? true
-      : [process.env.CLIENT_HOST].filter((host): host is string =>
-          Boolean(host),
-        ),
+  origin: true,
   credentials: true,
   optionsSuccessStatus: 200,
 };
