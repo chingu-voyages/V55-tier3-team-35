aa73f0b41f6bb8bcef37849c0951555b130e6638
kayoMichael <michaelxkayo@hotmail.com>
2025-06-06 21:52:27 -0400

    Add console log

--- apps/server/src/controllers/auth.controller.ts
+++ apps/server/src/controllers/auth.controller.ts
@@ -13,6 +13,7 @@ export const registerUser = async (
   next: NextFunction,
 ): Promise<void> => {
   try {
+    console.log('calling register');
     const validationResult = registerSchema.safeParse(req.body);
     if (!validationResult.success) {
       res.status(400).json({
